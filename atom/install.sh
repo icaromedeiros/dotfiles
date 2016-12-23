fb0743d0212ddadca279c064934a627065155cb7
Icaro Medeiros <icaro.medeiros@gmail.com>
2016-12-23 15:48:06 -0200

    fixed symlink for .atom, add jellybeans theme

--- atom/install.sh
+++ atom/install.sh
@@ -2,6 +2,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Symlinking .atom"
-ln -sf $DIR/.atom ~/.atom
+ln -sf $DIR/.atom ~/
 
+echo "[WARNING] Now open atom, install package-sync and run Package Sync: sync on Command Palette"
+echo "[WARNING] Then, restart Atom..."
 echo "Done."
